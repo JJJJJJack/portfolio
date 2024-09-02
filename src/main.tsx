f2be712eb1e4a08a2f6f3f497c329fc33709f476
JJJJJJack <97jack@live.it>
2024-09-03 01:14:23 +0200

    HashRouter for handling sharing URLs with different routes than "/"

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
